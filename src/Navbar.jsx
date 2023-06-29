d53af0998eff6cb565895089ada841bbbe15a28a
Gino Longobucco <gino_longob@hotmail.com>
2023-06-28 23:34:56 -0300

    subiendo los primeros archivos

---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Navbar;
